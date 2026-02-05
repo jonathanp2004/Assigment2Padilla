daf64ccfaa4caaa3c113ddff84483bdfbb9694f6
jonathanp2004 <jpadilla1782.2@gmail.com>
2026-02-05 17:51:39 -0600

    finished and tested Shell Sort (Yesterday), finsihed and tested QuickSort.

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,4 +1,62 @@
+import java.util.Arrays;
+
 public class QuickSort {
+
+
+    //should rearrange so all i < pivot to left and pivot, and if i > pivot should be put on the left
+
+    private int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+
+                arr[i] = arr[j];
+
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = pivot;
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public int[] sorty(int[] input){
+
+        quickSort(input, 0, input.length-1);
+        return input;
+    }
+
+    private void quickSort(int[] arr, int low, int high){
+
+        if(low < high){
+
+            int pivot = partition(arr, low, high);
+            quickSort(arr,low, pivot -1);
+            quickSort(arr, pivot +1, high);
+        }
+
+    }
+
+    public static void main(String[] args){
+
+    QuickSort quick = new QuickSort();
+
+
+    int[] test = {8,4,5,9,3,2,1};
+
+    System.out.println(Arrays.toString(quick.sorty(test)));
+
+
+    }
 
 
 }
---
+++ src/ShellSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class ShellSort {
+
+    private int[] sorty(int[] input){
+
+
+
+        for(int gap = input.length/2; gap > 0; gap/=2 ){
+            for(int i = gap; i < input.length; i++){
+                int temp = input[i];
+                int j = i;
+
+                while (j >= gap && input[j-gap] > temp) {
+
+                    input[j] = input[j -gap];
+                    j -= gap;
+                }
+                input[j] = temp;
+            }
+        }
+        return input;
+    }
+
+
+    public static void main (String[] args){
+
+        ShellSort sort = new ShellSort();
+
+        int[] test = {3,2,4,1,6,4};
+
+        System.out.println(Arrays.toString(sort.sorty(test)));
+
+    }
+}
